65ff8bb17e54c4652fb2a1166ac66d57fbede879
LAPTOP-F25A12VH\Dear <natthaphong.ea@gmail.com>
2024-07-02 22:36:37 +0700

    feat(Frontend Part): add attaching JWT Token in Request Header

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./index.css";
-import { AuthProvider } from "./contexts/authentication";
-import { BrowserRouter } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import { AuthProvider } from './contexts/authentication';
+import { BrowserRouter } from 'react-router-dom';
+import jwtInterceptor from './utils/jwtInterceptor';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+jwtInterceptor();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <AuthProvider>
--- client/src/utils/jwtInterceptor.js
+++ client/src/utils/jwtInterceptor.js
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios from 'axios';
 
 function jwtInterceptor() {
   axios.interceptors.request.use((req) => {
@@ -6,6 +6,15 @@ function jwtInterceptor() {
     //  ให้เขียน Logic ในการแนบ Token เข้าไปใน Header ของ Request
     // เมื่อมีการส่ง Request จาก Client ไปหา Server
     // ภายใน Callback Function axios.interceptors.request.use
+    const token = window.localStorage.getItem('token');
+    const hasToken = Boolean(token);
+
+    if (hasToken) {
+      req.headers = {
+        ...req.headers,
+        Authorization: `Bearer ${token}`
+      };
+    }
 
     return req;
   });
@@ -15,10 +24,18 @@ function jwtInterceptor() {
       return req;
     },
     (error) => {
+      console.log(error.response);
       // 🐨 Todo: Exercise #6
       //  ให้เขียน Logic ในการรองรับเมื่อ Server ได้ Response กลับมาเป็น Error
       // โดยการ Redirect ผู้ใช้งานไปที่หน้า Login และลบ Token ออกจาก Local Storage
       // ภายใน Error Callback Function ของ axios.interceptors.response.use
+      if (
+        error.response.status === 401 &&
+        error.response.statusText === 'Unauthorized'
+      ) {
+        window.localStorage.removeItem('token');
+        window.location.replace('login');
+      }
 
       return Promise.reject(error);
     }
